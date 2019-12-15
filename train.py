bde9a6f2bd1e7a322b31459ba841dfdb7a076492
Romaric Tsopnang <tsopnangsr@gmail.com>
2019-12-15 13:40:25 +0100

    Fixing Training issues with Script

--- train.py
+++ train.py
@@ -16,15 +16,16 @@ from collections import OrderedDict
 parser = argparse.ArgumentParser(description='Flower Classification')
 parser.add_argument('--data_dir', type=str, default='flowers', help='dataset directory')
 parser.add_argument('--save_dir', type=str, default='checkpoint.pth', help='saved checkpoint')
-parser.add_argument('--gpu', type=bool, default=False, help='to use GPU')
-parser.add_argument('--arch', type=str, default='VGG', help='architecture')
-parser.add_argument('--learning_rate', type=float, default=0.0002, help='learning rate')
+parser.add_argument('--gpu', type=bool, default=True, help='to use GPU')
+parser.add_argument('--arch', type=str, default='vgg19', help='architecture')
+parser.add_argument('--learning_rate', type=float, default=0.001, help='learning rate')
 parser.add_argument('--hidden_units', type=int, default=512, help='initial hidden units')
-parser.add_argument('--epochs', type=int, default=3, help='training epochs')
+parser.add_argument('--epochs', type=int, default=1, help='training epochs')
 
 args = parser.parse_args()
 
 data_dir = args.data_dir
+node_hidden = args.hidden_units
 
 train_dir = data_dir + '/train'
 valid_dir = data_dir + '/valid'
@@ -76,26 +77,87 @@ dataloaders['valid'] = valid_dataloaders
 dataloaders['test']  = test_dataloaders
 
 # TODO: Build the model
-model = models.vgg19(pretrained=True)
-num_features = model.classifier[0].in_features
+arch=str(args.arch)     #'vgg19'   #ok    #'resnet152'   #ok     #densenet161' ok      #'alexnet' ok
 
-# Freeze parameters
-for param in model.parameters():
-    param.requires_grad = False
+if(arch=='vgg19'):
+    model = models.vgg19(pretrained=True)
+    input_size = model.classifier[0].in_features
+    hidden_sizes = [node_hidden]
+    output_size = 102
 
-# Build classifier
-classifier = nn.Sequential(OrderedDict([
-                          ('fc1', nn.Linear(num_features, 1024)),
-                          ('relu', nn.ReLU()),
-                          ('dropout', nn.Dropout(p=0.3)),
-                          ('fc2', nn.Linear(1024, args.hidden_units)),
-                          ('relu', nn.ReLU()),
-                          ('dropout', nn.Dropout(p=0.3)),
-                          ('fc3', nn.Linear(args.hidden_units, 102)),
-                          ('output', nn.LogSoftmax(dim=1))
-                          ]))
+    # Freeze parameters
+    for param in model.parameters():
+        param.requires_grad = False
 
-model.classifier = classifier
+    # Build classifier
+    classifier = nn.Sequential(OrderedDict([
+                              ('fc1', nn.Linear(input_size, hidden_sizes[0])),
+                              ('relu1', nn.ReLU()),
+                              ('fc2', nn.Linear(hidden_sizes[0], output_size)),
+                              ('output', nn.LogSoftmax(dim=1))
+                              ]))
+
+    model.classifier = classifier
+
+
+if(arch=='resnet152'):
+    model = models.resnet152(pretrained=True)
+    input_size = 2048
+    hidden_sizes = [node_hidden]
+    output_size = 102
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+
+    classifier = nn.Sequential(OrderedDict([
+                              ('fc1', nn.Linear(input_size, hidden_sizes[0])),
+                              ('relu1', nn.ReLU()),
+                              ('fc2', nn.Linear(hidden_sizes[0], output_size)),
+                              ('output', nn.LogSoftmax(dim=1))
+                              ]))
+
+    model.fc = classifier   #last layer of resnet called fc not classifier so removing model.classifier with model.fc
+
+if(arch=='alexnet'):
+    model = models.alexnet(pretrained=True)
+    input_size = 9216
+    hidden_sizes = [node_hidden]
+    output_size = 102
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+
+    classifier = nn.Sequential(OrderedDict([
+                              ('fc1', nn.Linear(input_size, hidden_sizes[0])),
+                              ('relu1', nn.ReLU()),
+                              ('fc2', nn.Linear(hidden_sizes[0], output_size)),
+                              ('output', nn.LogSoftmax(dim=1))
+                              ]))
+
+    model.classifier = classifier   #last layer of resnet called fc not classifier so removing model.classifier with model.fc
+
+
+if(arch=='densenet161'):
+    model = models.densenet161(pretrained=True)
+    input_size = 2208
+    hidden_sizes = [node_hidden]
+    output_size = 102
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+
+    classifier = nn.Sequential(OrderedDict([
+                              ('fc1', nn.Linear(input_size, hidden_sizes[0])),
+                              ('relu1', nn.ReLU()),
+                              ('fc2', nn.Linear(hidden_sizes[0], output_size)),
+                              ('output', nn.LogSoftmax(dim=1))
+                              ]))
+
+    model.classifier = classifier   #last layer of resnet called fc not classifier so removing model.classifier with model.fc
+
 
 # Set negative log loss as the criteria
 criterion = nn.NLLLoss()
@@ -169,7 +231,7 @@ total = 0
 model.eval()
 
 with torch.no_grad():
-    for data in test_loader:
+    for data in test_dataloaders:
         images, labels = data
         images, labels = images.to(device), labels.to(device)
         outputs = model(images)
@@ -183,7 +245,7 @@ print('Accuracy of the network: {}' .format(valid / total))
 model.class_to_idx = train_datasets.class_to_idx
 
 check_point = {'arch':args.arch,
-              'input':num_features,
+              'input':input_size,
               'output':102,
               'epochs':args.epochs,
               'learning_rate':args.learning_rate,
@@ -193,4 +255,5 @@ check_point = {'arch':args.arch,
               'state_dict':model.state_dict(),
               'optimizer':optimizer.state_dict(),
               'class_to_idx': model.class_to_idx}
+
 torch.save(check_point, args.save_dir)
